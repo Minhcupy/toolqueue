0fd2292ff18dc811037dadd4268ab86a93761055
Minhcupy <tqmminh2004tqm@gmail.com>
2026-08-08 15:05:24 +0700

    Initial commit

---
+++ src/main/java/com/example/toolqueue/ToolqueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.toolqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolqueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolqueueApplication.class, args);
+    }
+
+}
